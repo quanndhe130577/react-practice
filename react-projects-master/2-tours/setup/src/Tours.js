54dbf955ad0a68e55da32f2f53a0b66c2443e462
DucQuan <quanhien1999@gmail.com>
2021-01-05 11:31:01 +0700

    Update Lesson 2 (Tours )and 3 (Reviews)

--- react-projects-master/2-tours/setup/src/Tours.js
+++ react-projects-master/2-tours/setup/src/Tours.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, removeTour}) => {
+
+  return (
+    <section>
+      <div className="title">
+        <h2>ours tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((item) => {
+          return <Tour key={item.id} {...item} removeTour={removeTour} />
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
--- react-projects-master/3-reviews/setup/src/App.js
+++ react-projects-master/3-reviews/setup/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Review from './Review';
+import { BsFillAlarmFill } from "react-icons/bs";
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review></Review>
+      </section>
+    </main>
+  );
 }
 
 export default App;
