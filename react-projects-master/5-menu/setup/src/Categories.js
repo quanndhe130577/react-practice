5222ab506944a91b5eb3435d6be80a0f02073642
DucQuan <quanhien1999@gmail.com>
2021-01-08 16:53:43 +0700

    Practice 4-5-6

--- react-projects-master/5-menu/setup/src/Categories.js
+++ react-projects-master/5-menu/setup/src/Categories.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({listCategories, filterItems}) => {
+  return (
+    <div className='btn-container'>
+      {
+        listCategories.map((category, index) => {
+          return (
+            <button key={index} className='filter-btn' 
+              onClick={() => {
+                filterItems(category);
+              }}>
+              {category}
+            </button>
+          );
+        })
+      }
+    </div>
+  );
 };
 
 export default Categories;
