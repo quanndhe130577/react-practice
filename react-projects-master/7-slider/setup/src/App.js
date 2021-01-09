b98007276a9a493628107ea7c7cbde80b11cff0d
DucQuan <quanhien1999@gmail.com>
2021-01-09 10:59:56 +0700

    ex 7 : slider. finished

--- react-projects-master/7-slider/setup/src/App.js
+++ react-projects-master/7-slider/setup/src/App.js
@@ -5,6 +5,25 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  //const {id, image, name, title, quote} = people[index];  
+
+  useEffect(() => {
+    if (index < 0) {
+      setIndex(people.length - 1);
+    }
+    if(index >= people.length){
+      setIndex(0);
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let inter = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000)
+    return () => clearInterval(inter);
+  }, [index])
+
   return <section className='section'>
     <div className='title'>
       <h2>
@@ -13,11 +32,23 @@ function App() {
     </div>
     <div className='section-center'>
       {
-        people.map((person, index) => {
-          const {id, image, name, title, quote} = person;
+        people.map((person, indexPerson) => {
+          const {id, image, name, title, quote} = person;          
           // more stuff coming up
+          let position = '';
+          //let position = indexPerson === index ? 'activeSlide' : indexPerson === index - 1 ? 'lastSlide' : indexPerson === index + 1 ? 'nextSlide' : '';
+          if(indexPerson === index){
+            position = 'activeSlide';
+          }else if(indexPerson === index + 1 || (indexPerson === 0 && index === people.length - 1)){
+            position = 'nextSlide';
+          }else if(indexPerson === index - 1 || (indexPerson === people.length - 1  && index === 0 )){
+            position = 'lastSlide';
+          }
+          if(position === ''){
+            return;
+          }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className='person-img' />
               <h4>{name}</h4>
               <p className='title'>{title}</p>
@@ -27,10 +58,33 @@ function App() {
           );
         })
       }
-      <button className='prev'>
+      {
+        /*
+        <article className='activeSlide' key={id}>
+        <img src={image} alt={name} className='person-img' />
+        <h4>{name}</h4>
+        <p className='title'>{title}</p>
+        <p className='text'>{quote}</p>
+        <FaQuoteRight className='icon'/>
+      </article>
+      */
+      }
+      <button className='prev' onClick={() => {
+        if(index === 0){
+          setIndex(people.length - 1);
+        }else{
+          setIndex(index-1);
+        }    
+      }}>
         <FiChevronLeft />
       </button>
-      <button className='next'>
+      <button className='next' onClick={() => {
+        if(index === people.length - 1){
+          setIndex(0);
+        }else{
+          setIndex(index+1);
+        }
+      }}>
         <FiChevronRight />
       </button>
     </div>
