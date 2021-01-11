af54358ea1187e8532f8d0e255e7ce0f67330a32
DucQuan <quanhien1999@gmail.com>
2021-01-11 14:41:11 +0700

    update ex 8 : lorem-ipsum : generate number of paragraphs

--- react-projects-master/8-lorem-ipsum/setup/src/App.js
+++ react-projects-master/8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,40 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
   
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    //setText(data.filter((value, index, arr) => index < amount));
+    setText(data.slice(0, amount));
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>
+          paragraphs:
+        </label>
+        <input type='number' min='0' max={data.length} name='amount' id='amount' 
+          value={count}
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        ></input>
+        <button type='submit' className='btn'>Generate</button>
+      </form>
+      <article className='lorem-text'>
+          {
+            text.map((item, index) => {
+              return <p key={index}>{item}</p>
+            })
+          }
+      </article>
+    </section>
+  );
 }
 
 export default App;
