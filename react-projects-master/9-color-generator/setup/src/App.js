705bf1a138b2325ca2e7acde40e652cd9209d2ae
DucQuan <quanhien1999@gmail.com>
2021-01-11 17:02:15 +0700

    update ex 9 : color generator

--- react-projects-master/9-color-generator/setup/src/App.js
+++ react-projects-master/9-color-generator/setup/src/App.js
@@ -1,10 +1,72 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SingleColor from './SingleColor'
 
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('#f15025');
+  const [error, setError] = useState(false);
+  const [indexCopy, setIndexCopy] = useState(-1);
+  const [step, setStep] = useState(10);
+  const [list, setList] = useState(new Values('#f15025').all(step));
+
+  const handleSubmit = e => {
+    
+    e.preventDefault();
+    updateList();
+    setIndexCopy(-1);
+  }
+
+  const updateList = () => {
+    try{
+      let colors = new Values(color).all(parseInt(step));
+      setList(colors);
+      setError(false);
+    }catch(error){
+      setError(true);
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    updateList();
+  }, [step])
+
+  const saveToClipBoard = (index) => {
+    setIndexCopy(index);
+  }
+
+  return (
+    <main>
+      <section  className='container'>
+        <h3>color generate</h3>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type='text' 
+            value={color} 
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+            placeholder='#f15025'
+            className={`${error?'error':null}`}>           
+          </input>
+          <input value={step} type='number' min='1' style={{margin: 10}} placeholder={`${step}%`}
+            onChange={(e) => {     
+              setStep(e.target.value);
+            }}
+          ></input>
+          <button className='btn' type='submit'>Submit</button>
+        </form>
+      </section>
+      <section className='colors'>
+        {
+          list.map((color, index) => {
+            return <SingleColor saveToClipBoard={saveToClipBoard} indexCopy={indexCopy} key={index} {...color} index={index} hexColor={color.hex}/>
+          })
+        }
+      </section>
+    </main>
+  );
 }
 
 export default App
--- react-projects-master/9-color-generator/setup/src/SingleColor.js
+++ react-projects-master/9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb, weight, index, hexColor, saveToClipBoard, indexCopy}) => {
+  //const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+
+  /*useEffect(() => {
+    const timout = setTimeout( () => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timout);
+  }, [alert])*/
+
+  return (
+    <article className={`color ${index > 10  && 'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}}
+      onClick={() => {
+        //setAlert(true);
+        saveToClipBoard(index);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {
+        index == indexCopy && <p className='alert'>copied to cliboard</p>
+      }
+    </article>
+  )
 }
 
 export default SingleColor
