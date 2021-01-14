51d9a212b97c7f977b5f46d8b26382a24b654d0b
DucQuan <quanhien1999@gmail.com>
2021-01-14 10:31:18 +0700

    finish ex13: stripe

--- react-projects-master/13-stripe-submenus/setup/src/Hero.js
+++ react-projects-master/13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import {useGlobalContext} from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {closeSubmenu} = useGlobalContext();
+
+  return (
+    <section className='hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Chau len ba cha di mau giao
+          </p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} className='phone-img' alt='phone'></img>
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
--- react-projects-master/13-stripe-submenus/setup/src/context.js
+++ react-projects-master/13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,60 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import sublinks from './data'
+
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+    const [location, setLocation] = useState({});
+
+    const [page, setPage] = useState({page: '', links: []});
+
+    const [listPage, setListPage] = useState([]);
+
+    useEffect(() => {
+        const newList = sublinks.map((item) => item.page);
+        setListPage(newList);
+    }, [])
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((item) => item.page === text);
+        setPage(page);
+        setLocation(coordinates);
+        setIsSubmenuOpen(true);   
+    }
+
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    }
+
+    return <AppContext.Provider
+        value={{
+            isSidebarOpen,
+            isSubmenuOpen,
+            openSidebar,
+            closeSidebar,
+            openSubmenu,
+            closeSubmenu,
+            location,
+            page,
+            listPage
+        }}
+    >
+        {children}
+    </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
