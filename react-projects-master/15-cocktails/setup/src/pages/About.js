9d5f7e64b2966cfd8cb82c24676116de02d977de
DucQuan <quanhien1999@gmail.com>
2021-01-18 17:56:22 +0700

    finish ex15: Cocktail using 'react-router-dom', useCallback

--- react-projects-master/15-cocktails/setup/src/pages/About.js
+++ react-projects-master/15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>Chau len ba chau di mau giao</p>
+    </section>
   )
 }
 
